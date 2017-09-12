f9555d3aacb71618ca06b4550fa874e4fea9f37e
Alex Slaughter <alex@alexslaughter.com>
2017-09-13 02:13:48 +0300

    working but slow

--- sample4.py
+++ sample4.py
@@ -2,75 +2,107 @@ import numpy as np
 import cv2
 import imutils
 from matplotlib import pyplot as plt
+import os
 
-isPassing = False
-
-def render(image):
-    center_x = 909 * .6
-    center_y = 751 * .6
+def render(image, center_x, center_y):
+    scaler = .6
+    center_x = center_x * scaler
+    center_y = center_y * scaler
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (0, 0), fx=0.6, fy=0.6)
-    # gray = gray[250:500, 0:1920]
+    gray = cv2.resize(gray, (0, 0), fx=scaler, fy=scaler)
 
     lsd = cv2.createLineSegmentDetector(0)
     lines = lsd.detect(gray)[0]
     filtered_lines = []
 
-    xs = []
     ys = []
 
-    def isCentered(m, l):
+    def isValid(m, l):
 
         if l[1] < center_y or l[3] < center_y:
             return False
 
+        height = abs(l[3] - l[1])
+        width = abs(l[2] - l[0])
         dx = (center_x - l[0])
         dy = abs(center_y - l[1])
-        slope =  dx/dy
+        slope =  dx / dy
 
-        # print("dx: {:f}, dy: {:f}".format(dx, dy))
-        # print("real slope: {:f}, expected slope: {:f}".format(m, slope))
-        # cv2.line(gray, (int(l[0]),int(l[1])), (int(center_x),int(center_y)), (255,0,0), 1)
+        if not dy > 0:
+            return False
 
-        # Check result vs expected is within 10%
-        return (abs(1 - m/slope) * 100) < 10
+        if height < 20 or height > 50 or width > 100:
+            return False
 
+        diff = abs(m - slope)
+        # print("dx: {:f}. dy: {:f}, actual: {:f}, derived: {:f}, diff: {:f}".format(height, width, m, slope, diff))
+
+        return diff < .35
+
+    # print("---------------------------------------------------")
     for line in lines:
         l = line[0]
 
         dx = l[0] - l[2] if l[1] < l[3] else l[2] - l[0]
         dy = abs(l[3] - l[1])
 
-        if dy > 40:
+        if dy > 0:
             slope = dx / dy
-            # print("x: {:f},  y: {:f}, slope: {:f}".format(dx, dy, slope))
-            # filtered_lines.append(line)
-            if isCentered(slope, l):
+            if isValid(slope, l):
+                # print("dx: {:f}. dy: {:f}, slope: {:f}".format(dx, dy, slope))
                 filtered_lines.append(line)
+                ys.append(min(l[1], l[3]))
 
-        # if y_d < x_d and y_d < 10 and x_d > 20 and x_d < 60:
-        #     print("{:f} - {:f}".format(x_d, y_d))
-        #     filtered_lines.append(line)
+    if len(filtered_lines) > 7:
+        # print("We're passing")
+        ys.sort(key=int)
+        # print(ys)
+        y_delta = abs(ys[0] - ys[-1])
+        # print(y_delta)
+        if(y_delta < 70):
+            return True
 
-    if len(filtered_lines) > 4:
-        isPassing = True
-        print("We're passing")
+    # print("Number of lines: {:d}".format(len(filtered_lines)))
+    # drawn_img = lsd.drawSegments(gray,np.array(filtered_lines))
 
-    print("Number of lines: {:d}".format(len(filtered_lines)))
-    drawn_img = lsd.drawSegments(gray,np.array(filtered_lines))
+    # Draw focal point
     # cv2.circle(drawn_img,(int(center_x),int(center_y)), 20, (0,0,255), -1)
+    # cv2.imshow("Image", drawn_img)
+    # cv2.waitKey(0)
 
-    cv2.imshow("Image", drawn_img)
-    cv2.waitKey(0)
 
-cap = cv2.VideoCapture('zebra/akn.031.037.left.avi')
-while(cap.isOpened() and isPassing is False):
-    print("Continue {:b}".format(isPassing))
-    ret, frame = cap.read()
-    render(frame)
-cap.release()
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
 
-# image = cv2.imread('sample-img-2.png')
-# render(image)
+files = []
+for file in os.listdir("zebra"):
+    if file.endswith(".avi"):
+        files.append(os.path.join("zebra", file))
+
+# files = ["zebra/akn.088.141.left.avi"]
+index = 0
+for file_name in files:
+    text_file = "{:s}.txt".format(file_name[:-9])
+    center_x, center_y = file_get_contents(text_file).split(" ")
+    center_x = int(center_x)
+    center_y = int(center_y)
+
+    cap = cv2.VideoCapture(file_name)
+    print("Reading file {:s}".format(file_name))
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        index += 1
+        # if index != 24: continue
+        # if index != 628: continue
+        # print("Index Number: {:d}".format(index))
+        if render(frame, center_x, center_y):
+            print("TRUE")
+            break
+    cap.release()
+
 cv2.destroyAllWindows()
